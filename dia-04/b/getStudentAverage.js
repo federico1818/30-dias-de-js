85f86588e4a557542bc0d71bab99680b06e2f5d8
Federico González <federico1818@gmail.com>
2023-03-03 23:59:48 -0300

    dia 04 - getStudentAverage

--- dia-04/b/getStudentAverage.js
+++ dia-04/b/getStudentAverage.js
@@ -1,11 +1,20 @@
 export function getStudentAverage(students) {
+    let total = 0
+    let size = 0
+
     students = students.map(student => {
+        let sum = student.grades.reduce((sum, grade) => sum + grade, 0)
+        total += sum
+        size += student.grades.length
         return {
-            ...student,
-            sum: student.grades.reduce((sum, grade) => sum + grade, 0),
-            average: + (student.sum / student.grades.length).toFixed(2)
+            name: student.name,
+            average: + (sum / student.grades.length).toFixed(2)
         }
     })
-    let classAverage = students.reduce((sum, student) => sum + student.sum, 0) 
+
+    return {
+        classAverage: + (total / size).toFixed(2),
+        students: students,
+    }
 }
   
