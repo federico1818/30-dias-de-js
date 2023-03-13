cd42bcbe50be0cc99e8fff7fc6e35dc972c3b679
Federico González <federico1818@gmail.com>
2023-03-13 20:26:03 -0300

    dia 11 - runCode - tasks

---
+++ dia-11/b/tasks.js
@@ -0,0 +1,18 @@
+export function doTask1() {
+    return new Promise((resolve, reject) => {
+        window.setTimeout(() => resolve('Task 1'), 300)
+    })
+}
+
+export function doTask2() {
+    return new Promise((resolve, reject) => {
+        window.setTimeout(() => resolve('Task 2'), 300)
+    })
+}
+
+export function doTask3() {
+    return new Promise((resolve, reject) => {
+        window.setTimeout(() => resolve('Task 3'), 300)
+    })
+}
+  
